05a69622db87c019efed277a1a93d99700f7cf36
SHAKHZOD <shoxzodohunjonov@gmail.com>
2025-07-25 18:20:15 +0500

    leetcode_clone

--- app/components/Modals/Login.tsx
+++ app/components/Modals/Login.tsx
@@ -4,7 +4,7 @@ type LoginProps = {};
 
 const Login:React.FC<LoginProps> = () => {
     
-    return <form className='space-y-6 px-6 pb-4'>
+    return     <form className='space-y-6 px-6 pb-4'>
         <h3 className='text-xl font-medium text-white'>Sign in to LeetCode</h3>
     <div>
         <label htmlFor='email' className='text-sm font-medium block mb-2 text-gray-300'>
@@ -15,12 +15,26 @@ const Login:React.FC<LoginProps> = () => {
                 bg-gray-600 border-gray-500 placeholder:bg-gray-400 text-white' placeholder='shahzodohunjon@gmail.com'/>
     </div>
     <div>
-        <label htmlFor='email' className='text-sm font-medium block mb-2 text-gray-300'>
-            Your Email
+        <label htmlFor='password' className='text-sm font-medium block mb-2 text-gray-300'>
+            Your password
         </label>
-        <input type='email' name='email' id='email' className='
+        <input type='password' name='password' id='password' className='
                 border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
-                bg-gray-600 border-gray-500 placeholder:bg-gray-400 text-white' placeholder='shahzodohunjon@gmail.com'/>
+                bg-gray-600 border-gray-500 placeholder:bg-gray-400 text-white' placeholder='********'/>
+    </div>
+
+    <button type='submit' className='w-full text-white focus:ring-blue-300 font-medium rounded-lg
+          text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s'>
+        Login
+    </button>
+    <button className='flex w-full justify-end'>
+        <a href='#' className='text-sm block text-brand-orange hover:underline w-full text-right'></a>
+        Forgot password?
+    </button>
+    <div className='text-sm font-medium text-gray-300'>
+        Not Regitered?
+        <a href='#' className='text-blue-700 hover:underline'></a>
+        Create account
     </div>
     </form>
 }
